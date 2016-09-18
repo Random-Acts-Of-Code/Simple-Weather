32f27734d39c42c656c443c2bfd72b47237ee7a8
Collin Makohon <cmakohon@me.com>
2016-09-17 23:52:26 -0400

    added basic angular functionality

--- app/app.js
+++ app/app.js
@@ -1,3 +1,14 @@
 /**
  * Created by cmakohon on 9/17/16.
  */
+
+var app1 = angular.module('app1', []);
+
+app1.controller('ctrl1', function($scope) {
+
+    $scope.zipcity = '';
+
+    $scope.find = function() {
+        $scope.result = $scope.zipcity;
+    };
+});
